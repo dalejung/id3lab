61a69a824f7085d78f33ad905f01d19ac0805f0e
Dale Jung <dale@dalejung.com>
2013-09-07 23:35:17 -0400

    converted notebook repr to use Javascript

--- id3lab/__init__.py
+++ id3lab/__init__.py
@@ -20,14 +20,37 @@ class ID3Lab(tslab.Lab):
         name = self.get_varname()
         return sa.link(name)
 
-    def _repr_html_(self):
-        out = '<strong>stations:</strong> <br />'
-        out += '<br />'.join(self.stations.keys())
+    def _repr_javascript_(self):
+        """
+        Output:
+            
+        stations:
+        AAPL
+        IBM
+
+        <link>
+
+        Note:
+            Uses Javascript because sa.link() returns javascript. This is 
+            because sa.link needs to use the location.href to get the notebook_id
+            since we cannot grab that from within the notebook kernel.
+        """
+        js = """ 
+        element.append('{0}');
+        container.show();
+        """
+        station_text = '<strong>stations:</strong> <br />'
+        station_text += '<br />'.join(self.stations.keys())
+        out = js.format(station_text)
+
         link = self.link().data
-        out += '<br />' + link
+        out += 'elemenet.append("<br />");' + link
         return out
 
     @property
     def html_obj(self):
         # for now default to sharedx
         return sharedx
+
+    def __str__(self):
+        return 
